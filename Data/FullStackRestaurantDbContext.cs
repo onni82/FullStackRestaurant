e95d38fae45f83dc12c80e14482d78322eb25da2
onni82 <onni.bucht@me.com>
2025-08-19 12:52:29 +0200

    Updated DbContext

--- Data/FullStackRestaurantDbContext.cs
+++ Data/FullStackRestaurantDbContext.cs
@@ -1,6 +1,20 @@
-﻿namespace FullStackRestaurant.Data
+﻿using FullStackRestaurant.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStackRestaurant.Data
 {
-	public class FullStackRestaurantDbContext
+	public class FullStackRestaurantDbContext : DbContext
 	{
+		public FullStackRestaurantDbContext(DbContextOptions<FullStackRestaurantDbContext> options)
+			: base(options) { }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			// Default 2-hour booking
+			modelBuilder.Entity<Booking>()
+				.Property(b => b.End)
+				.HasComputedColumnSql("[Start] + '02:00:00'", stored: true);
+		}
 	}
 }
