0d3091568c6f707aa4282a57f1dd5b942c82d770
onni82 <onni.bucht@me.com>
2025-08-20 11:40:02 +0200

    Added API controllers

---
+++ Controllers/MenuItemsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FullStackRestaurant.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemsController : ControllerBase
+    {
+    }
+}
