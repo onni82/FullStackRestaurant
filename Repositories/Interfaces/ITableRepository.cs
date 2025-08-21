f688d74c13ffcaccc8a20eb164522a25d065b3a4
onni82 <onni.bucht@me.com>
2025-08-21 14:46:07 +0200

    Added Repositories

---
+++ Repositories/Interfaces/ITableRepository.cs
@@ -0,0 +1,12 @@
+﻿using FullStackRestaurant.Models;
+
+namespace FullStackRestaurant.Repositories.Interfaces
+{
+    public interface ITableRepository
+    {
+        Task<IEnumerable<Table>> GetAllAsync();
+        Task<Table> GetByIdAsync(int id);
+        Task<Table> CreateAsync(Table table);
+        Task<bool> DeleteAsync(int id);
+    }
+}
