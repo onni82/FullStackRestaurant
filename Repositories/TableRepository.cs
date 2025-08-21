95d30121d69680a4d7092d19cd0c5769265ee78d
onni82 <onni.bucht@me.com>
2025-08-21 15:05:21 +0200

    Implemented GetAllAsync()

--- Repositories/TableRepository.cs
+++ Repositories/TableRepository.cs
@@ -1,6 +1,22 @@
-﻿namespace FullStackRestaurant.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using FullStackRestaurant.Data;
+using FullStackRestaurant.Models;
+using FullStackRestaurant.Repositories.Interfaces;
+
+namespace FullStackRestaurant.Repositories
 {
-    public class TableRepository
+    public class TableRepository : ITableRepository
     {
+        private readonly FullStackRestaurantDbContext _context;
+
+        public TableRepository(FullStackRestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Table>> GetAllAsync()
+        {
+            return await _context.Tables.ToListAsync();
+        }
     }
 }
