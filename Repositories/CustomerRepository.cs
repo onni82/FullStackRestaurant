201c9006994f4119b87c3c913850655bf902063a
onni82 <onni.bucht@me.com>
2025-08-21 16:38:09 +0200

    Changed to tabs

--- Repositories/CustomerRepository.cs
+++ Repositories/CustomerRepository.cs
@@ -9,35 +9,35 @@ namespace FullStackRestaurant.Repositories
 	{
 		private readonly FullStackRestaurantDbContext _context;
 
-        public CustomerRepository(FullStackRestaurantDbContext context)
-        {
-            _context = context;
-        }
+		public CustomerRepository(FullStackRestaurantDbContext context)
+		{
+			_context = context;
+		}
 
-        public async Task<IEnumerable<Customer>> GetAllAsync()
-        {
-            return await _context.Customers.ToListAsync();
-        }
+		public async Task<IEnumerable<Customer>> GetAllAsync()
+		{
+			return await _context.Customers.ToListAsync();
+		}
 
-        public async Task<Customer?> GetByIdAsync(int id)
-        {
-            return await _context.Customers.FindAsync(id);
-        }
+		public async Task<Customer?> GetByIdAsync(int id)
+		{
+			return await _context.Customers.FindAsync(id);
+		}
 
-        public async Task<Customer> CreateAsync(Customer customer)
-        {
-            _context.Customers.Add(customer);
-            await _context.SaveChangesAsync();
-            return customer;
-        }
-        public async Task<bool> DeleteAsync(int id)
-        {
-            var customer = await _context.Customers.FindAsync(id);
-            if (customer == null) { return false; }
+		public async Task<Customer> CreateAsync(Customer customer)
+		{
+			_context.Customers.Add(customer);
+			await _context.SaveChangesAsync();
+			return customer;
+		}
+		public async Task<bool> DeleteAsync(int id)
+		{
+			var customer = await _context.Customers.FindAsync(id);
+			if (customer == null) { return false; }
 
-            _context.Customers.Remove(customer);
-            await _context.SaveChangesAsync();
-            return true;
-        }
-    }
+			_context.Customers.Remove(customer);
+			await _context.SaveChangesAsync();
+			return true;
+		}
+	}
 }
