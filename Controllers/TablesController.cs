70e9a4a31ef18b11ec58dbaaffb8968b9644667a
onni82 <onni.bucht@me.com>
2025-08-21 16:02:38 +0200

    Changed to tabs

--- Controllers/TablesController.cs
+++ Controllers/TablesController.cs
@@ -13,39 +13,39 @@ namespace FullStackRestaurant.Controllers
 	{
 		private readonly ITableService _tableService;
 
-        public TablesController(ITableService tableService)
-        {
-            _tableService = tableService;
-        }
+		public TablesController(ITableService tableService)
+		{
+			_tableService = tableService;
+		}
 
 		[HttpGet]
-        public async Task<ActionResult<IEnumerable<TableDTO>>> GetAll()
-        {
-            var tables = await _tableService.GetAllAsync();
-            return Ok(tables);
-        }
+		public async Task<ActionResult<IEnumerable<TableDTO>>> GetAll()
+		{
+			var tables = await _tableService.GetAllAsync();
+			return Ok(tables);
+		}
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<TableDTO>> GetById(int id)
-        {
-            var table = await _tableService.GetByIdAsync(id);
-            if (table == null) { return NotFound(); }
-            return Ok(table);
-        }
+		[HttpGet("{id}")]
+		public async Task<ActionResult<TableDTO>> GetById(int id)
+		{
+			var table = await _tableService.GetByIdAsync(id);
+			if (table == null) { return NotFound(); }
+			return Ok(table);
+		}
 
-        [HttpPost]
-        public async Task<ActionResult<TableDTO>> Create(CreateTableDTO dto)
-        {
-            var created = await _tableService.CreateAsync(dto);
-            return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
-        }
+		[HttpPost]
+		public async Task<ActionResult<TableDTO>> Create(CreateTableDTO dto)
+		{
+			var created = await _tableService.CreateAsync(dto);
+			return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
+		}
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var deleted = await _tableService.DeleteAsync(id);
-            if (!deleted) { return NotFound(); }
-            return NoContent();
-        }
-    }
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> Delete(int id)
+		{
+			var deleted = await _tableService.DeleteAsync(id);
+			if (!deleted) { return NotFound(); }
+			return NoContent();
+		}
+	}
 }
