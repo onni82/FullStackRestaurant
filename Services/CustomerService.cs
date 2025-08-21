046b0f19710852f88d04dccb2ac0c92e8ec0d5d1
onni82 <onni.bucht@me.com>
2025-08-21 16:50:08 +0200

    Changed to tabs

--- Services/CustomerService.cs
+++ Services/CustomerService.cs
@@ -6,59 +6,59 @@ using FullStackRestaurant.Services.Interfaces;
 namespace FullStackRestaurant.Services
 {
 	public class CustomerService : ICustomerService
-    {
-        private readonly ICustomerRepository _customerRepository;
+	{
+		private readonly ICustomerRepository _customerRepository;
 
-        public CustomerService(ICustomerRepository customerRepository)
-        {
-            _customerRepository = customerRepository;
-        }
+		public CustomerService(ICustomerRepository customerRepository)
+		{
+			_customerRepository = customerRepository;
+		}
 
-        public async Task<IEnumerable<CustomerDTO>> GetAllAsync()
-        {
-            var customers = await _customerRepository.GetAllAsync();
-            return customers.Select(c => new CustomerDTO
-            {
-                Id = c.Id,
-                Name = c.Name,
-                Phone = c.Phone
-            });
-        }
+		public async Task<IEnumerable<CustomerDTO>> GetAllAsync()
+		{
+			var customers = await _customerRepository.GetAllAsync();
+			return customers.Select(c => new CustomerDTO
+			{
+				Id = c.Id,
+				Name = c.Name,
+				Phone = c.Phone
+			});
+		}
 
-        public async Task<CustomerDTO?> GetByIdAsync(int id)
-        {
-            var customer = await _customerRepository.GetByIdAsync(id);
-            if (customer == null) { return null; }
+		public async Task<CustomerDTO?> GetByIdAsync(int id)
+		{
+			var customer = await _customerRepository.GetByIdAsync(id);
+			if (customer == null) { return null; }
 
-            return new CustomerDTO
-            {
-                Id = customer.Id,
-                Name = customer.Name,
-                Phone = customer.Phone
-            };
-        }
+			return new CustomerDTO
+			{
+				Id = customer.Id,
+				Name = customer.Name,
+				Phone = customer.Phone
+			};
+		}
 
-        public async Task<CustomerDTO> CreateAsync(CreateCustomerDTO dto)
-        {
-            var customer = new Customer
-            {
-                Name = dto.Name,
-                Phone = dto.Phone
-            };
+		public async Task<CustomerDTO> CreateAsync(CreateCustomerDTO dto)
+		{
+			var customer = new Customer
+			{
+				Name = dto.Name,
+				Phone = dto.Phone
+			};
 
-            var created = await _customerRepository.CreateAsync(customer);
+			var created = await _customerRepository.CreateAsync(customer);
 
-            return new CustomerDTO
-            {
-                Id = created.Id,
-                Name = created.Name,
-                Phone = created.Phone
-            };
-        }
+			return new CustomerDTO
+			{
+				Id = created.Id,
+				Name = created.Name,
+				Phone = created.Phone
+			};
+		}
 
-        public async Task<bool> DeleteAsync(int id)
-        {
-            return await _customerRepository.DeleteAsync(id);
-        }
-    }
+		public async Task<bool> DeleteAsync(int id)
+		{
+			return await _customerRepository.DeleteAsync(id);
+		}
+	}
 }
