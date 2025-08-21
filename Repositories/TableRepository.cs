0b4e28c10046f0cc2d5f041bfddbd6f5ddb4b027
onni82 <onni.bucht@me.com>
2025-08-21 15:16:27 +0200

    Implemented rest of interface methods

--- Repositories/TableRepository.cs
+++ Repositories/TableRepository.cs
@@ -18,5 +18,27 @@ namespace FullStackRestaurant.Repositories
 		{
 			return await _context.Tables.ToListAsync();
 		}
+
+		public async Task<Table?> GetByIdAsync(int id)
+		{
+			return await _context.Tables.FindAsync(id);
+		}
+
+		public async Task<Table> CreateAsync(Table table)
+		{
+			_context.Tables.Add(table);
+			await _context.SaveChangesAsync();
+			return table;
+		}
+
+		public async Task<bool> DeleteAsync(int id)
+		{
+			var table = await _context.Tables.FindAsync(id);
+			if (table == null) { return false; }
+
+			_context.Tables.Remove(table);
+			await _context.SaveChangesAsync();
+			return true;
+		}
 	}
 }
