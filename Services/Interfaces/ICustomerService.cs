0bffeb283d4ea3f023af561a4fe2e7c0d7f65c90
onni82 <onni.bucht@me.com>
2025-08-21 16:40:36 +0200

    Created interface

--- Services/Interfaces/ICustomerService.cs
+++ Services/Interfaces/ICustomerService.cs
@@ -1,6 +1,12 @@
-﻿namespace FullStackRestaurant.Services.Interfaces
+﻿using FullStackRestaurant.DTOs;
+
+namespace FullStackRestaurant.Services.Interfaces
 {
 	public interface ICustomerService
 	{
+		Task<IEnumerable<CustomerDTO>> GetAllAsync();
+		Task<CustomerDTO?> GetByIdAsync(int id);
+		Task<CustomerDTO> CreateAsync(CustomerDTO dto);
+		Task<bool> DeleteAsync(int id);
 	}
 }
