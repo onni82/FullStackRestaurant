b944bca0bd46ef9474abe7be902030cf7832eced
onni82 <onni.bucht@me.com>
2025-08-21 16:59:40 +0200

    Added methods

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FullStackRestaurant.DTOs;
+using FullStackRestaurant.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FullStackRestaurant.Controllers
@@ -7,5 +9,41 @@ namespace FullStackRestaurant.Controllers
 	[ApiController]
 	public class CustomersController : ControllerBase
 	{
+		private readonly ICustomerService _customerService;
+
+		public CustomersController(ICustomerService customerService)
+		{
+			_customerService = customerService;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<CustomerDTO>>> GetAll()
+		{
+			var customers = await _customerService.GetAllAsync();
+			return Ok(customers);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<CustomerDTO>> GetById(int id)
+		{
+			var customer = await _customerService.GetByIdAsync(id);
+			if (customer == null) { return NotFound(); }
+			return Ok(customer);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<CustomerDTO>> Create(CreateCustomerDTO dto)
+		{
+			var created = await _customerService.CreateAsync(dto);
+			return CreatedAtAction(nameof(GetById), new { id = created.Id}, created);
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> Delete(int id)
+		{
+			var deleted = await _customerService.DeleteAsync(id);
+			if (!deleted) { return NotFound(); }
+			return NoContent();
+		}
 	}
 }
