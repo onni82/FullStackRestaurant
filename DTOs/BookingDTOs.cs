5535392ebfbed049dc7c2c7dc33ec4a07d7c309b
onni82 <onni.bucht@me.com>
2025-08-22 14:57:11 +0200

    Renamed PartySize to Guests

--- DTOs/BookingDTOs.cs
+++ DTOs/BookingDTOs.cs
@@ -3,19 +3,18 @@
 	public class BookingDTO
 	{
 		public int Id { get; set; }
-		public DateTime Start { get; set; }
-		public DateTime End { get; set; }
-		public int PartySize { get; set; }
-
 		public int TableId { get; set; }
 		public string CustomerId { get; set; }
+		public DateTime Start { get; set; }
+		public DateTime End { get; set; }
+		public int Guests { get; set; }
 	}
 
 	public class CreateBookingDTO
 	{
-		public DateTime Start { get; set; }
-		public int PartySize { get; set; }
 		public int TableId { get; set; }
 		public string CustomerId { get; set; }
+		public DateTime Start { get; set; }
+		public int Guests { get; set; }
 	}
 }
