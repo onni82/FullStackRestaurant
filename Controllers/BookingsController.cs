b2f912cc42b1e8ed0cf5adaf3f2e380b0558eaf1
onni82 <onni.bucht@me.com>
2025-08-22 16:34:12 +0200

    Added methods

--- Controllers/BookingsController.cs
+++ Controllers/BookingsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FullStackRestaurant.DTOs;
+using FullStackRestaurant.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FullStackRestaurant.Controllers
@@ -7,5 +9,40 @@ namespace FullStackRestaurant.Controllers
 	[ApiController]
 	public class BookingsController : ControllerBase
 	{
+		private readonly IBookingService _bookingService;
+
+		public BookingsController(IBookingService bookingService)
+		{
+			_bookingService = bookingService;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<BookingDTO>>> GetAll()
+		{
+			var bookings = await _bookingService.GetAllAsync();
+			return Ok(bookings);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<BookingDTO>> GetById(int id)
+		{
+			var booking = await _bookingService.GetByIdAsync(id);
+			if (booking == null) { return NotFound(); }
+			return Ok(booking);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<BookingDTO>> Create(CreateBookingDTO dto)
+		{
+			var booking = await _bookingService.CreateAsync(dto);
+			return CreatedAtAction(nameof(GetById), new { id = booking.Id }, booking);
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> Delete(int id)
+		{
+			var deleted = await _bookingService.DeleteAsync(id);
+			return deleted ? NoContent() : NotFound();
+		}
 	}
 }
