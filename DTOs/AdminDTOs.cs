7b01c78c668cc0bcd23e192fefbc3c9f508028c5
onni82 <onni.bucht@me.com>
2025-08-22 20:18:50 +0200

    Added LoginDTO

--- DTOs/AdminDTOs.cs
+++ DTOs/AdminDTOs.cs
@@ -5,7 +5,14 @@
 		public int Id { get; set; }
 		public string Username { get; set; } = string.Empty;
 	}
+
 	public class CreateAdminDTO
+	{
+		public string Username { get; set; } = string.Empty;
+		public string Password { get; set; } = string.Empty;
+	}
+
+	public class LoginDTO
 	{
 		public string Username { get; set; } = string.Empty;
 		public string Password { get; set; } = string.Empty;
