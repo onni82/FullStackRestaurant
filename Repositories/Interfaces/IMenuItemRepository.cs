a3cc813c289093985ef4e69eda21a385a2ba215d
onni82 <onni.bucht@me.com>
2025-08-22 16:56:26 +0200

    Added methods

--- Repositories/Interfaces/IMenuItemRepository.cs
+++ Repositories/Interfaces/IMenuItemRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace FullStackRestaurant.Repositories.Interfaces
+﻿using FullStackRestaurant.Models;
+
+namespace FullStackRestaurant.Repositories.Interfaces
 {
 	public interface IMenuItemRepository
 	{
+		Task<IEnumerable<MenuItem>> GetAllAsync();
+		Task<MenuItem?> GetByIdAsync(int id);
+		Task<MenuItem> CreateAsync(MenuItem menuItem);
+		Task<bool> DeleteAsync(int id);
 	}
 }
--- Repositories/MenuItemRepository.cs
+++ Repositories/MenuItemRepository.cs
@@ -1,6 +1,44 @@
-﻿namespace FullStackRestaurant.Repositories
+﻿using FullStackRestaurant.Data;
+using FullStackRestaurant.Models;
+using FullStackRestaurant.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStackRestaurant.Repositories
 {
-	public interface MenuItemRepository
+	public class MenuItemRepository : IMenuItemRepository
 	{
+		private readonly FullStackRestaurantDbContext _context;
+
+		public MenuItemRepository(FullStackRestaurantDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IEnumerable<MenuItem>> GetAllAsync()
+		{
+			return await _context.MenuItems.ToListAsync();
+		}
+
+		public async Task<MenuItem?> GetByIdAsync(int id)
+		{
+			return await _context.MenuItems.FindAsync(id);
+		}
+
+		public async Task<MenuItem> CreateAsync(MenuItem menuItem)
+		{
+			_context.MenuItems.Add(menuItem);
+			await _context.SaveChangesAsync();
+			return menuItem;
+		}
+
+		public async Task<bool> DeleteAsync(int id)
+		{
+			var menuItem = await _context.MenuItems.FindAsync(id);
+			if (menuItem == null) { return false; }
+
+			_context.MenuItems.Remove(menuItem);
+			await _context.SaveChangesAsync();
+			return true;
+		}
 	}
 }
