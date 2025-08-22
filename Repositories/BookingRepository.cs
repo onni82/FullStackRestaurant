9b34b8bee68263b569ee93e835d00baf3c690044
onni82 <onni.bucht@me.com>
2025-08-22 15:15:11 +0200

    Added methods to IBookingRepository and BookingRepository

--- Repositories/BookingRepository.cs
+++ Repositories/BookingRepository.cs
@@ -1,6 +1,50 @@
-﻿namespace FullStackRestaurant.Repositories
+﻿using FullStackRestaurant.Data;
+using FullStackRestaurant.Models;
+using FullStackRestaurant.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStackRestaurant.Repositories
 {
-	public class BookingRepository
+	public class BookingRepository : IBookingRepository
 	{
+		private readonly FullStackRestaurantDbContext _context;
+
+		public BookingRepository(FullStackRestaurantDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IEnumerable<Booking>> GetAllAsync()
+		{
+			return await _context.Bookings
+				.Include(b => b.Table)
+				.Include(b => b.Customer)
+				.ToListAsync();
+		}
+
+		public async Task<Booking?> GetByIdAsync(int id)
+		{
+			return await _context.Bookings
+				.Include(b => b.Table)
+				.Include(b => b.Customer)
+				.FirstOrDefaultAsync(b => b.Id == id);
+		}
+
+		public async Task<Booking> CreateAsync(Booking booking)
+		{
+			_context.Bookings.Add(booking);
+			await _context.SaveChangesAsync();
+			return booking;
+		}
+
+		public async Task<bool> DeleteAsync(int id)
+		{
+			var booking = await _context.Bookings.FindAsync(id);
+			if (booking == null) { return false; }
+
+			_context.Bookings.Remove(booking);
+			await _context.SaveChangesAsync();
+			return true;
+		}
 	}
 }
--- Repositories/Interfaces/IBookingRepository.cs
+++ Repositories/Interfaces/IBookingRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace FullStackRestaurant.Repositories.Interfaces
+﻿using FullStackRestaurant.Models;
+
+namespace FullStackRestaurant.Repositories.Interfaces
 {
 	public interface IBookingRepository
 	{
+		Task<IEnumerable<Booking>> GetAllAsync();
+		Task<Booking?> GetByIdAsync(int id);
+		Task<Booking> CreateAsync(Booking booking);
+		Task<bool> DeleteAsync();
 	}
 }
