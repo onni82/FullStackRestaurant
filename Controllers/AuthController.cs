7a0ec46fad5af43610d44bdb316beeb2e150a7b4
onni82 <onni.bucht@me.com>
2025-08-25 16:30:30 +0200

    Added AuthController

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using FullStackRestaurant.DTOs;
+using FullStackRestaurant.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FullStackRestaurant.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthController : ControllerBase
+	{
+		private readonly IAdminService _adminService;
+        public AuthController(IAdminService adminService) => _adminService = adminService;
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO dto)
+		{
+            var res = await _adminService.LoginAsync(dto);
+            if (res is null) { return Unauthorized(new { message = "Invalid username or password" }); }
+            return Ok(res);
+        }
+    }
+}
