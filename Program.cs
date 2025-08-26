c2b92f900666fd20d1b3c951bf8dfc6e147085ff
onni82 <onni.bucht@me.com>
2025-08-26 13:58:54 +0200

    Added scoped repositories and services

--- Program.cs
+++ Program.cs
@@ -19,8 +19,20 @@ namespace FullStackRestaurant
 			{
 				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 			});
+
+			// Scoped repositories
 			builder.Services.AddScoped<ITableRepository, TableRepository>();
-			builder.Services.AddScoped<ITableService, TableService>();
+			builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+			builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+			builder.Services.AddScoped<IMenuItemRepository, MenuItemRepository>();
+			builder.Services.AddScoped<IAdminRepository, AdminRepository>();
+
+            // Scoped services
+            builder.Services.AddScoped<ITableService, TableService>();
+            builder.Services.AddScoped<ICustomerService, CustomerService>();
+            builder.Services.AddScoped<IBookingService, BookingService>();
+            builder.Services.AddScoped<IMenuItemService, MenuItemService>();
+            builder.Services.AddScoped<IAdminService, AdminService>();
 			builder.Services.AddScoped<IJwtService, JwtService>();
 
 			// Add services to the container.
