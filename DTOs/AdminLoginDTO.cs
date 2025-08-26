780fe0e2b1b9b3c2810bd4dc0152e5547ccb174b
onni82 <onni.bucht@me.com>
2025-08-26 09:10:37 +0200

    added AdminLoginDTO

---
+++ DTOs/AdminLoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace FullStackRestaurant.DTOs
+{
+	public class AdminLoginDTO
+	{
+		public string Username { get; set; } = string.Empty;
+		public string Password { get; set; } = string.Empty;
+	}
+}
