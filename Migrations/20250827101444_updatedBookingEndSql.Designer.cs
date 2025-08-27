fe2727314815ff4933ed023da460f25a138b2c75
onni82 <onni.bucht@me.com>
2025-08-27 12:15:30 +0200

    added updated booking end sql computation migration

--- Migrations/20250827101444_updatedBookingEndSql.Designer.cs
+++ Migrations/20250827101444_updatedBookingEndSql.Designer.cs
@@ -4,6 +4,7 @@ using FullStackRestaurant.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FullStackRestaurant.Migrations
 {
     [DbContext(typeof(FullStackRestaurantDbContext))]
-    partial class FullStackRestaurantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250827101444_updatedBookingEndSql")]
+    partial class updatedBookingEndSql
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
